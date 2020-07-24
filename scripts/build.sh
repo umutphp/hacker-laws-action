4358a8fcfd35864b0ab70386d129f4162a3eb7b1
Umut Işık <umutphp@gmail.com>
2020-07-25 02:12:39 +0300

    Minor

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ curl -s https://api.github.com/repos/umutphp/hacker-laws-cli/releases/latest \
 | grep "browser_download_url.*linux_amd64*.tar.gz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -q -
+| wget -q 
 
 tar -xzvf hacker-laws-cli*.tar.gz
 
