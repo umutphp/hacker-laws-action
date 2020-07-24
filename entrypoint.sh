066b7ac437c5956b9eef44ecd1e0b51736f98541
Umut Işık <umutphp@gmail.com>
2020-07-25 01:47:54 +0300

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -l
+
+hacker-laws-cli random > /law.txt
+
+curl -X POST $PR_URL -H "Content-Type: application/json" \
+            -H "Authorization: token $GITHUB_TOKEN" \
+            ---data-binary "@/law.txt"
---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+curl -s https://api.github.com/repos/umutphp/hacker-laws-cli/releases/latest \
+| grep "browser_download_url.*linux_amd64*.tar.gz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+tar -xzvf hacker-laws-cli*.tar.gz
+
+cp hacker-laws-cli /
