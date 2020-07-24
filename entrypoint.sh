034ec2e37359ddad129c15cda1ec105d8203b184
Umut Işık <umutphp@gmail.com>
2020-07-25 02:34:13 +0300

    Minor

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ hacker-laws-cli random > /law.txt
 
 curl -X POST $PR_URL -H "Content-Type: application/json" \
             -H "Authorization: token $GITHUB_TOKEN" \
-            ---data-binary "@/law.txt"
+            --data-binary "@/law.txt"
