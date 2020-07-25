39e34541aca500e282dea9e5006c1796bca2a9ae
Umut Işık <umutphp@gmail.com>
2020-07-25 18:31:19 +0300

    Minor

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ SENDER+="${PR_SENDER}"
 
 echo $SENDER
 
-echo '{ "body": "Hi ${SENDER}, This comment is a small gift to you for opening the PR. Here is a random law from [Hacker Laws repository](https://github.com/dwmkerr/hacker-laws).' > /law_json.txt
+echo '{ "body": "Hi {$SENDER}, This comment is a small gift to you for opening the PR. Here is a random law from [Hacker Laws repository](https://github.com/dwmkerr/hacker-laws).' > /law_json.txt
 cat /law_escaped.txt >> /law_json.txt
 echo '" }' >> /law_json.txt
 
