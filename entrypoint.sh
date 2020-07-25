3626c735cce5a4b0d60a5a2df7034becd05e3352
Umut Işık <umutphp@gmail.com>
2020-07-25 03:10:11 +0300

    Mnior

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ echo '{ "body": "' > /law_json.txt
 cat /law_escaped.txt >> /law_json.txt
 echo '" }' >> /law_json.txt
 
+sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' /law_json.txt > /law_single_line.txt
+
+
 cat /law_json.txt
 
 curl -X POST $PR_URL -H "Content-Type: application/octet-stream" \
             -H "Authorization: token $GITHUB_TOKEN" \
-            --data-binary "@/law_json.txt"
+            --data-binary "@/law_single_line.txt"
