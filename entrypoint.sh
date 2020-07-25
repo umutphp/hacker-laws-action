3a0a7a08b8497eec7ae135dffa4736e7e8da3802
Umut Işık <umutphp@gmail.com>
2020-07-25 16:57:50 +0300

    Minor

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,11 @@
 
 sed 's/\"/\\\"/g' /law.txt > /law_escaped.txt
 
-echo '{ "body": "This comment is a small gift to you for opening the PR. Here is a random law from Hacker Laws repo.' > /law_json.txt
+echo '{ "body": "This comment is a small gift to you for opening the PR. Here is a random law from [Hacker Laws repository](https://github.com/dwmkerr/hacker-laws).' > /law_json.txt
 cat /law_escaped.txt >> /law_json.txt
 echo '" }' >> /law_json.txt
 
-sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' /law_json.txt > /law_single_line.txt
-
-sed 's/github.com\/dwmkerr\/hacker-laws/https:\/\/github.com\/dwmkerr\/hacker-laws/g' /law_single_line.txt > /law_final.txt
+sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' /law_json.txt > /law_final.txt
 
 curl -X POST $PR_URL -H "Content-Type: application/octet-stream" \
     -H "Authorization: token $GITHUB_TOKEN" \
